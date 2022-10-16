71fd869065210e5e4f4e60cf9c6d1bb5f4104337
BOSTONE069 <bostoneochieng69@gmail.com>
2022-10-16 10:03:51 -0400

    SSH configuration file for the local SSH client

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,12 +1,12 @@
 # Setting up ssh config file using puppet
 
-file {'ssh_config':
+file {'ssh_config_first_work':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => 'IdentityFile ~/.ssh/school',
   multiple => true,
 }
-file {'ssh_config':
+file {'ssh_config_second_work':
   ensure   => present,
   path     => '/etc/ssh/ssh_config',
   line     => 'PasswordAuthentication no',
